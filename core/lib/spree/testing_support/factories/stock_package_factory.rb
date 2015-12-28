ccd0dfd8544e62043937b3b446a3ed80505c6c8c
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 23:39:14 +0100

    Split stock factory into stock packer and stock package factory

--- core/lib/spree/testing_support/factories/stock_package_factory.rb
+++ core/lib/spree/testing_support/factories/stock_package_factory.rb
@@ -1,14 +1,4 @@
 FactoryGirl.define do
-  # must use build()
-  factory :stock_packer, class: Spree::Stock::Packer do
-    transient do
-      stock_location { build(:stock_location) }
-      contents []
-    end
-
-    initialize_with { new(stock_location, contents) }
-  end
-
   factory :stock_package, class: Spree::Stock::Package do
     transient do
       stock_location { build(:stock_location) }
